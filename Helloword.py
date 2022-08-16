019cf9f8ce96b3508131f661d54341f59496aecc
jinwoojeon111 <90844415+jinwoojeon111@users.noreply.github.com>
2022-08-16 13:21:35 +0900

    bfs문제1번

---
+++ Helloword.py
@@ -0,0 +1,28 @@
+from collections import deque
+def solution(begin, target, words):
+    answer = 0
+    queue = deque()
+    queue.append((begin,0))
+    visited = {}
+    for word in words:
+        visited[word] = False
+    
+    while queue:
+        x, step = queue.popleft()
+        if x == target:
+            return step
+        for word in words:
+            if visited[word]:
+                continue
+            cnt = 0
+            for i in range(len(begin)):
+                if x[i] == word[i]:
+                    cnt +=1
+            if cnt == len(begin)-1:
+                visited[word] = True
+                queue.append((word,step+1))
+    
+    print(answer)
+    return 0
+
+solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"])
